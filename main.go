a5d9cd3d141ad0f856923adc3886d7e504fa37c9
Janos Gyerik <janos@kronos>
2016-02-29 22:55:25 +0100

    added todo to main and removed some garbage

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"github.com/janosgyerik/go-practice/dupfinder/dupfinder"
-)
+// TODO
+// take list of files from stdin
+// print out duplicates visually grouped
 
 func main() {
-	fmt.Println(dupfinder.Compare("/tmp/main.go", "/tmp/main.go"))
-	fmt.Println(dupfinder.Compare("/tmp/main.go", "/tmp/a"))
-	fmt.Println(dupfinder.Compare("/tmp/a", "/tmp/main.go"))
 }
-
