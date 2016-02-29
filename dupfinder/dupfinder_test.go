e051ebb23f18b89e92e69699b19d642f4492ad55
Janos Gyerik <janos@kronos>
2016-02-29 22:51:51 +0100

    TestCompare_same_size_content_descending and asc

--- dupfinder/dupfinder_test.go
+++ dupfinder/dupfinder_test.go
@@ -20,32 +20,62 @@ func TestCompare_same_file(t*testing.T) {
 	}
 }
 
-func TestCompare_shorter_size_to_longer_size(t*testing.T) {
-	shorter := "/tmp/a"
-	longer := "/tmp/main.go"
+func TestCompare_size_ascending(t*testing.T) {
+	smaller := "/tmp/a"
+	bigger := "/tmp/main.go"
 
 	expected := -1
 
-	cmp, err := Compare(shorter, longer)
+	cmp, err := Compare(smaller, bigger)
 	if err != nil {
-		t.Errorf("Compare(shorter, longer) raised error: %v", err)
+		t.Errorf("Compare(smaller, bigger) raised error: %v", err)
 	}
 	if cmp != expected {
-		t.Errorf("Compare(shorter, longer) == %v, want %v", cmp, expected)
+		t.Errorf("Compare(smaller, bigger) == %v, want %v", cmp, expected)
 	}
 }
 
-func TestCompare_longer_size_to_shorter_size(t*testing.T) {
-	shorter := "/tmp/a"
-	longer := "/tmp/main.go"
+func TestCompare_size_descending(t*testing.T) {
+	smaller := "/tmp/a"
+	bigger := "/tmp/main.go"
 
 	expected := 1
 
-	cmp, err := Compare(longer, shorter)
+	cmp, err := Compare(bigger, smaller)
 	if err != nil {
-		t.Errorf("Compare(longer, shorter) raised error: %v", err)
+		t.Errorf("Compare(bigger, smaller) raised error: %v", err)
 	}
 	if cmp != expected {
-		t.Errorf("Compare(longer, shorter) == %v, want %v", cmp, expected)
+		t.Errorf("Compare(bigger, smaller) == %v, want %v", cmp, expected)
+	}
+}
+
+func TestCompare_same_size_content_ascending(t*testing.T) {
+	lower := "/tmp/a"
+	higher := "/tmp/b"
+
+	expected := -1
+
+	cmp, err := Compare(lower, higher)
+	if err != nil {
+		t.Errorf("Compare(lower, higher) raised error: %v", err)
+	}
+	if cmp != expected {
+		t.Errorf("Compare(lower, higher) == %v, want %v", cmp, expected)
+	}
+}
+
+func TestCompare_same_size_content_descending(t*testing.T) {
+	lower := "/tmp/a"
+	higher := "/tmp/b"
+
+	expected := 1
+
+	cmp, err := Compare(higher, lower)
+	if err != nil {
+		t.Errorf("Compare(higher, lower) raised error: %v", err)
+	}
+	if cmp != expected {
+		t.Errorf("Compare(higher, lower) == %v, want %v", cmp, expected)
 	}
 }
