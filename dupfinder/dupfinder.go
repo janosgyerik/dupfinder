33ba801a9e26efcfd598c14b233b17b55c7bb102
Janos Gyerik <janos@kronos>
2016-02-29 23:20:52 +0100

    simplfied using a chunker

--- dupfinder/dupfinder.go
+++ dupfinder/dupfinder.go
@@ -2,20 +2,23 @@ package dupfinder
 
 import (
 	"os"
-	"io"
 	"bytes"
 )
 
 const chunkSize = 64000
 
-func compareInts(a, b int) int {
-	if a == b {
-		return 0
+func chunker(f *os.File, ch chan []byte) {
+	for {
+		buf := make([]byte, chunkSize)
+		_, err := f.Read(buf)
+
+		if err != nil {
+			close(ch)
+			return
+		}
+
+		ch <- buf
 	}
-	if a < b {
-		return -1
-	}
-	return 1
 }
 
 func Compare(path1, path2 string) (int, error) {
@@ -31,29 +34,22 @@ func Compare(path1, path2 string) (int, error) {
 		return 0, err
 	}
 
+	ch1 := make(chan []byte)
+	ch2 := make(chan []byte)
+
+	go chunker(fd1, ch1)
+	go chunker(fd2, ch2)
+
 	for {
-		buf1 := make([]byte, chunkSize)
-		count1, err := fd1.Read(buf1)
-		if err != nil && err != io.EOF{
-			return 0, err
-		}
-
-		buf2 := make([]byte, chunkSize)
-		count2, err := fd2.Read(buf2)
-		if err != nil && err != io.EOF {
-			return 0, err
-		}
-
-		if count1 != count2 {
-			return compareInts(count1, count2), nil
-		}
+		buf1 := <-ch1
+		buf2 := <-ch2
 
 		cmp := bytes.Compare(buf1, buf2)
 		if cmp != 0 {
 			return cmp, nil
 		}
 
-		if err == io.EOF {
+		if len(buf1) == 0 {
 			return 0, nil
 		}
 	}
