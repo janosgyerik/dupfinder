4b79c1ae20af049ff0590e6874a0b384bac40652
Janos Gyerik <janos@kronos>
2017-04-17 23:43:08 +0200

    Merge dup2 into dupfinder

--- dupfinder.go
+++ dupfinder.go
@@ -26,6 +26,180 @@ func chunker(r io.Reader, ch chan <- []byte) {
 	}
 }
 
+
+type Group struct {
+	Paths []string
+}
+
+type FileHandler interface {
+	Path() string
+
+	Size() int64
+
+	Digest() string
+
+	NewReadCloser() (io.ReadCloser, error)
+}
+
+type fileHandler struct {
+	path   string
+	size   int64
+	digest string
+}
+
+func NewFileHandler(path string, file os.FileInfo) FileHandler {
+	return fileHandler{
+		path: path,
+		size: file.Size(),
+		digest: string(file.Size()),
+	}
+}
+
+func (f fileHandler) Path() string {
+	return f.path
+}
+
+func (f fileHandler) Size() int64 {
+	return f.size
+}
+
+func (f fileHandler) Digest() string {
+	return f.digest
+}
+
+func (f fileHandler) NewReadCloser() (io.ReadCloser, error) {
+	return os.Open(f.Path())
+}
+
+type Tracker interface {
+	Add(FileHandler, FileHandler)
+
+	Groups() []Group
+}
+
+type simpleTracker struct {
+	groups map[string]Group
+}
+
+func (tracker *simpleTracker) Add(f1, f2 FileHandler) {
+	group, found := tracker.groups[f1.Path()]
+	if found {
+		group.Paths = append(group.Paths, f2.Path())
+	} else {
+		group = Group{Paths: []string{f1.Path(), f2.Path()}}
+	}
+	tracker.groups[f1.Path()] = group
+}
+
+func (tracker *simpleTracker) Groups() []Group {
+	groups := []Group{}
+	for _, group := range tracker.groups {
+		groups = append(groups, group)
+	}
+	return groups
+}
+
+func NewTracker() Tracker {
+	return &simpleTracker{
+		groups: make(map[string]Group),
+	}
+}
+
+type Index interface {
+	Add(FileHandler)
+
+	Groups() []Group
+}
+
+type Filter interface {
+	Match(base, other FileHandler) bool
+}
+
+type sizeFilter struct{}
+
+func (filter sizeFilter) Match(f FileHandler, other FileHandler) bool {
+	return f.Size() == other.Size()
+}
+
+type digestFilter struct{}
+
+func (filter digestFilter) Match(f FileHandler, other FileHandler) bool {
+	return f.Digest() == other.Digest()
+}
+
+type contentFilter struct{}
+
+// TODO return errors, let caller handle
+func (filter contentFilter) Match(f FileHandler, other FileHandler) bool {
+	fd1, err := f.NewReadCloser()
+	defer fd1.Close()
+	if err != nil {
+		return false
+	}
+
+	fd2, err := other.NewReadCloser()
+	defer fd2.Close()
+	if err != nil {
+		return false
+	}
+
+	cmp, err := CompareReaders(fd1, fd2)
+	if err != nil {
+		return false
+	}
+	return cmp == 0
+}
+
+type simpleIndex struct {
+	files   []FileHandler
+	filters []Filter
+	tracker Tracker
+}
+
+func (index *simpleIndex) Add(f FileHandler) {
+	files := index.files
+	for _, filter := range index.filters {
+		files = applyFilter(filter, files, f)
+	}
+
+	switch len(files) {
+	case 0:
+		index.files = append(index.files, f)
+	case 1:
+		index.tracker.Add(files[0], f)
+	default:
+		// TODO return error instead, let caller handle (+ unit test it)
+		panic("more than one duplicates found in the unique index")
+	}
+}
+
+func applyFilter(filter Filter, files []FileHandler, base FileHandler) []FileHandler {
+	filtered := []FileHandler{}
+	for _, file := range files {
+		if filter.Match(base, file) {
+			filtered = append(filtered, file)
+		}
+	}
+	return filtered
+}
+
+func (index *simpleIndex) Groups() []Group {
+	return index.tracker.Groups()
+}
+
+func NewIndex() Index {
+	index := simpleIndex{
+		files: []FileHandler{},
+		filters: []Filter{
+			sizeFilter{},
+			digestFilter{},
+			contentFilter{},
+		},
+		tracker: NewTracker(),
+	}
+	return &index
+}
+
 func CompareFiles(path1, path2 string) (int, error) {
 	fd1, err := os.Open(path1)
 	defer fd1.Close()
--- dupfinder_test.go
+++ dupfinder_test.go
@@ -6,7 +6,255 @@ import (
 	"io/ioutil"
 	"os"
 	"strconv"
+	"path"
+	"io"
+	"reflect"
 )
+
+const testDataDir = "testdata"
+
+var testFileCounter = 0
+
+func newIndex() Index {
+	return NewIndex()
+}
+
+func Test_tracker_add(t*testing.T) {
+	tracker := NewTracker()
+
+	if len(tracker.Groups()) != 0 {
+		t.Fatalf("got %#v groups from empty tracker", tracker.Groups())
+	}
+
+	f1 := newTestFile()
+	f2 := newTestFile()
+	tracker.Add(f1, f2)
+
+	if len(tracker.Groups()) != 1 {
+		t.Fatalf("got %d groups from tracker; expected 1", len(tracker.Groups()))
+	}
+
+	if x := len(tracker.Groups()[0].Paths); x != 2 {
+		t.Fatalf("got %d files in group; expected 2", x)
+	}
+
+	f3 := newTestFile()
+	tracker.Add(f1, f3)
+
+	if len(tracker.Groups()) != 1 {
+		t.Fatalf("got %d groups from tracker; expected 1", len(tracker.Groups()))
+	}
+
+	if x := len(tracker.Groups()[0].Paths); x != 3 {
+		t.Fatalf("got %d files in group; expected 3", x)
+	}
+
+	f4 := newTestFile()
+	f5 := newTestFile()
+	tracker.Add(f4, f5)
+
+	if len(tracker.Groups()) != 2 {
+		t.Fatalf("got %d groups from tracker; expected 2", len(tracker.Groups()))
+	}
+}
+
+type testFile struct {
+	path    string
+	size    int64
+	digest  string
+	content string
+}
+
+func newTestFile() testFile {
+	testFileCounter += 1
+
+	return testFile{
+		path: strconv.Itoa(testFileCounter),
+	}
+}
+
+func (f testFile) Path() string {
+	return f.path
+}
+
+func (f testFile) Size() int64 {
+	return f.size
+}
+
+func (f testFile) Digest() string {
+	return f.digest
+}
+
+func (f testFile) NewReadCloser() (io.ReadCloser, error) {
+	return testReadCloser{strings.NewReader(f.content)}, nil
+}
+
+type testReadCloser struct {
+	reader io.Reader
+}
+
+func (t testReadCloser) Read(buf []byte) (int, error) {
+	return t.reader.Read(buf)
+}
+
+func (t testReadCloser) Close() error {
+	return nil
+}
+
+func fileWithSize(size int64) FileHandler {
+	return testFile{size: size}
+}
+
+func fileWithDigest(digest string) FileHandler {
+	return testFile{digest: digest}
+}
+
+func Test_fileWithSize(t*testing.T) {
+	f1_1 := fileWithSize(1)
+	f1_2 := fileWithSize(1)
+	f2 := fileWithSize(2)
+
+	if f1_1.Size() != f1_2.Size() {
+		t.Fatal("got different size for two fileWithSize(1)")
+	}
+	if f1_1.Size() == f2.Size() {
+		t.Fatal("got same size for fileWithSize(1) and fileWithSize(2)")
+	}
+}
+
+func Test_fileWithDigest(t*testing.T) {
+	f1_1 := fileWithDigest("1")
+	f1_2 := fileWithDigest("1")
+	f2 := fileWithDigest("2")
+
+	if f1_1.Digest() != f1_2.Digest() {
+		t.Fatal("got different digest for two fileWithDigest(1)")
+	}
+	if f1_1.Digest() == f2.Digest() {
+		t.Fatal("got same digest for fileWithDigest(1) and fileWithDigest(2)")
+	}
+}
+
+func Test_should_not_find_duplicates_with_different_size(t*testing.T) {
+	index := newIndex()
+
+	index.Add(fileWithSize(1))
+	index.Add(fileWithSize(2))
+
+	if len(index.Groups()) > 0 {
+		t.Fatal("found duplicates in two files with different size")
+	}
+}
+
+func Test_should_not_find_duplicates_with_different_digest(t*testing.T) {
+	index := newIndex()
+
+	index.Add(fileWithDigest("1"))
+	index.Add(fileWithDigest("2"))
+
+	if len(index.Groups()) > 0 {
+		t.Fatal("found duplicates in two files with different digest")
+	}
+}
+
+func Test_should_find_duplicates_in_two_identical_files(t*testing.T) {
+	index := newIndex()
+
+	file := testFile{
+		size: 1,
+		digest: "1",
+	}
+	index.Add(file)
+	index.Add(file)
+
+	if len(index.Groups()) != 1 {
+		t.Fatal("did not find duplicates in two identical files")
+	}
+}
+
+func Test_should_find_duplicates_in_a_mix(t*testing.T) {
+	index := newIndex()
+
+	file1 := testFile{size: 1, digest: "1", content: "1"}
+	file2 := testFile{size: 1, digest: "1", content: "1"}
+	file3 := testFile{size: 1, digest: "1", content: "3"}
+
+	index.Add(file1)
+	index.Add(file2)
+	index.Add(file3)
+
+	if len(index.Groups()) != 1 {
+		t.Fatal("did not find duplicates in a mix of files")
+	}
+
+	actual := index.Groups()[0].Paths
+	expected := []string{file1.Path(), file2.Path()}
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("got %#v in duplicate group, expected %#v", actual, expected)
+	}
+}
+
+func Test_alldups(t*testing.T) {
+	basedir := path.Join(testDataDir, "alldups")
+	files, err := ioutil.ReadDir(basedir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	index := newIndex()
+	for _, file := range files {
+		p := path.Join(basedir, file.Name())
+		f := NewFileHandler(p, file)
+		index.Add(f)
+	}
+
+	if len(index.Groups()) != 1 {
+		t.Fatalf("got %d duplicate groups, expected 1", len(index.Groups()))
+	}
+
+	if x := len(index.Groups()[0].Paths); x != 3 {
+		t.Fatalf("got %d files in duplicate group, expected 3", x)
+	}
+}
+
+func Test_nodups(t*testing.T) {
+	basedir := path.Join(testDataDir, "nodups")
+	files, err := ioutil.ReadDir(basedir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	index := newIndex()
+	for _, file := range files {
+		p := path.Join(basedir, file.Name())
+		f := NewFileHandler(p, file)
+		index.Add(f)
+	}
+
+	if len(index.Groups()) != 0 {
+		t.Fatalf("got %d duplicate groups, expected none", len(index.Groups()))
+	}
+}
+
+func Test_samesize(t*testing.T) {
+	basedir := path.Join(testDataDir, "samesize")
+	files, err := ioutil.ReadDir(basedir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	index := newIndex()
+	for _, file := range files {
+		p := path.Join(basedir, file.Name())
+		f := NewFileHandler(p, file)
+		index.Add(f)
+	}
+
+	if len(index.Groups()) != 0 {
+		t.Fatalf("got %d duplicate groups, expected none", len(index.Groups()))
+	}
+}
 
 func TestCompareReaders_same_file(t*testing.T) {
 	content := "dummy content"
